5f7ea02a782fd6a0adb06226d69dd74c9942012b
DanielM <s224477774@deakin.edu.au>
2026-07-27 12:02:27 +1000

    Initial commit

---
+++ src/data.ts
@@ -0,0 +1,71 @@
+export const heroImage = "banner.jpg"
+export const welcomeText = "Hey I'm Daniel"
+export const profileImage = "me.png"
+export const profileText = "I am a third year IT student at Deakin University"
+export const projects = [
+  {
+    name: "VR Red Flags Project",
+    desc: "I was contracted to create a functional prototype for researchers at Deakin University over an incredibly tight 3 week deadline. The project was developed in Unity with the XR toolkit. I was working as a full-stack developer responsible for all aspects of the finished prototype.",
+    image: "/redflags.avif",
+  },
+  {
+    name: "1987 Toyota Celica Restoration",
+    desc: "In the few bits and pieces of spare time I get between university and work, I've been hard at work getting my 1987 Toyota Celica back on the road. It's had its ups and downs, and has turned out to be more work than I initially expected...",
+    image: "/Celica.JPG",
+  },
+]
+export const photos = [
+  "cat.png", "Trees.JPG", "Spire.png", "Celica1.jpg"
+]
+
+export const artcileText = "Articles"
+
+export const articles = [
+  {
+    name: "Super EuroBeat comes to Australia",
+    desc: "Super EuroBeat is now available in Australia and can be listened to on YouTube.",
+    image: "/front.png",
+    author: "Initial D Selection",
+    rating: 5
+  },
+  {
+    name: "Initial D Crash at the last moments!",
+    desc: "After a close race between the other guy in the '86, Takumi foolishly over-revved his 86 and blew his very expensive engine. However, with his quick thinking, he was able to hit the clutch and roll over the finish line in reverse first!",
+    image: "public/initialPEAK.png",
+    author: "Initial D Season 5",
+    rating: 5
+  },
+  {
+    name: "Oil Crisis Hits Critical Levels",
+    desc: "Terrible news, back in  May 2025, my really good oil method got patched out of RC2 and no longer works.",
+    image: "/barrel.png",
+    author: "IHYA Studios",
+    rating: 2
+  },
+]
+
+export const tutorialText = "Tutorials"
+
+export const tutorials = [
+  {
+    name: "Node JS",
+    desc: "Learn more about Node JS here!",
+    image: "/nodejs.png",
+    author: "Daniel Mc",
+    rating: 3.5
+  },
+  {
+    name: "CSS",
+    desc: "Learn about CSS here!",
+    image: "/css.webp",
+    author: "Daniel Mc",
+    rating: 4
+  },
+  {
+    name: "HTML",
+    desc: "Learn about HTML here!",
+    image: "/html.webp",
+    author: "Daniel Mc",
+    rating: 4
+  },
+]
